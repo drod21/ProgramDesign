d83794c1b42ef85090b475390e5104d522b5bb9f
Derek Rodriguez <drod2169@gmail.com>
2016-01-25 10:07:24 -0500

    Project 3  part 1, voltage increments done

--- Projects/Project3/voltage.c
+++ Projects/Project3/voltage.c
@@ -9,7 +9,25 @@
 
 int main(void) 
 {
-
-
-
+    double t, volts;
+    
+    printf("time (sec)\t voltage\n");
+    
+    for (t=0; t<=12; t+=0.5) {
+        
+        if (t < 1) {
+            volts = 0.5 * sin(2*t);
+        }
+        
+       if (1.0 <= t && t <= 10.0) {
+            volts = sin(t);
+        }
+        
+       if (t > 10.0) {
+            volts = sin(10.0);
+        }
+        
+        printf("%.2f\t%.2f\n", t, volts);
+    }
+    return 0;
 }
