0a6370c86bd8b6615cab24070f2a8e1ec41cc061
Derek Rodriguez <drod2169@gmail.com>
2016-01-25 05:41:56 -0500

    Project 3
    Begin Project 3 part 1 and also the test file.

---
+++ Projects/Project3/voltage.c
@@ -0,0 +1,15 @@
+/* Calculates voltage for an experiment that will be repeated 
+   using an AC voltage signal
+   Derek Rodriguez
+   U37516832
+   01/25/2016 */
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) 
+{
+
+
+
+}
