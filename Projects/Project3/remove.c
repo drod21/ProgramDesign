d3196d39e223a9623511b78975f01d957b1e16a7
Derek Rodriguez <drod2169@gmail.com>
2016-01-25 05:47:20 -0500

    Project 3 Part 2
    Lazily begin the second part of Project 3 (skeleton program right now)

---
+++ Projects/Project3/remove.c
@@ -0,0 +1,17 @@
+/* Derek Rodriguez **
+** U37516832 **
+** 01/25/2016 **
+*************
+** Takes a message entered by user **
+** Removes punctuation (nonletters) except white spaces **
+** Makes letters Uppercase, prints message **
+*/
+
+#include <stdio.h>
+
+int main(void) 
+{
+printf("Please enter a message.\n");
+scanf("", &);
+
+}
