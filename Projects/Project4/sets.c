dc21d765d3af1f3495ab35acaaba833d63c955a2
Derek Rodriguez <drod2169@gmail.com>
2016-02-05 06:01:20 -0500

    Update sets code with selection sort complete and comments

--- Projects/Project4/sets.c
+++ Projects/Project4/sets.c
@@ -1,10 +1,10 @@
-//
-//  sets.c
-//
-//
-//  Created by Derek Rodriguez on 2/1/16.
-//
-//
+/* Derek Rodriguez
+ * 2/5/2016
+ * U37516832
+ *
+ * Stores two user inputs into arrays (length the user defined) and compares the two sets
+ * for union and intersection
+ */
 
 #include <stdio.h>
 #define SIZE 10
@@ -18,26 +18,28 @@ int main(void) {
     
     printf("Please enter the number of elements in the first set:\n");
     scanf("%d", &elem_A);
-    printf("Please enter the numbers in the first set:\n");
+    
+    printf("Please enter the numbers in the first set:\n"); /* Check user input, set that equal to array size for set_A */
     for (i = 0; i < elem_A; i++) {
         scanf("%d", &set_A[i]);
     }
     printf("Please enter the number of elements in the second set:\n");
     scanf("%d", &elem_B);
     
-    printf("Please enter the numbers in the second set:\n");
+    printf("Please enter the numbers in the second set:\n"); /* Check user input, set that equal to array size for set_B */
     for (i = 0; i < elem_B; i++) {
         scanf("%d", &set_B[i]);
     }
     
-    //union
+    /* Union detection code */
     for (i = 0; i < elem_A; i++) {
         if (i < elem_A) {
-            union_array[k] = set_A[i];
+            union_array[k] = set_A[i]; /* Store set_A into union array if less than user input length */
             k++;
         }
     }
     
+    /* Comparison check for set B, along with variable f for true/false to store into union array */
     for (i = 0; i < elem_B; i++) {
         f = 0;
         
@@ -48,6 +50,7 @@ int main(void) {
             }
         }
         
+        /* Store the remaining variables from set B and increment counter */
         if (f == 0) {
             if (i < elem_B) {
                 union_array[k] = set_B[i];
@@ -55,40 +58,49 @@ int main(void) {
             }
         }
     }
-    //selection_sort(union_array, k);
-    printf("The union of the two sets is: { ");
+    selection_sort(union_array, k); /* Sort the numbers in the union array */
+    printf("The union of the two sets is: {"); /* Print pattern for union */
     for (i = 0; i < k; i++) {
         printf(" %d", union_array[i]);
     }
     
     printf(" }\n");
     
+    
+    /* Detect intersection */
     k = 0;
-    // intersection
-    for (j = 0; j < elem_A; j++) {
-        for (i = 0; i < elem_B; i++) {
-            if (set_B[i] == set_A[j]) {
+    for (j = 0; j < elem_A; j++) { /* For loop to compare element A */
+        for (i = 0; i < elem_B; i++) { /* Nested for loop for element B */
+            if (set_B[i] == set_A[j]) { /* If set B == set A, intersection */
                 break;
             }
         }
         
         if (i != elem_B) {
-            intersection_array[k] = set_B[i];
+            intersection_array[k] = set_B[i]; /* If i isn't in elem_b, add set_B[i] into intersection array, increase counter */
             k++;
         }
     }
-    //selection_sort(intersection_array, k);
+    
+    /* Only sort the intersection array if there are values present
+     * Otherwise, return empty set
+     */
+    if (intersection_array[k] != 0) {
+    selection_sort(intersection_array, k);
+    }
     printf("The intersection of the two sets is: { ");
     for (i = 0; i < k; i++) {
         printf(" %d", intersection_array[i]);
     }
     printf(" }\n");
+        
     return 0;
     
 }
 
-void selection_sort(int a[], int n)
-{
+/* Sort the integers by smallest -> largest */
+
+void selection_sort(int a[], int n) {
     int largest = 0, temp;
     
     if (n == 1)
@@ -105,9 +117,8 @@ void selection_sort(int a[], int n)
     
     selection_sort(a, n - 1);
 }
-
-int find_largest(int a[], int n)
-{
+/* Find the largest integer in the index */
+int find_largest(int a[], int n) {
     int largest_index = 0;
     int i;
     for (i = 1; i < n; i++)
