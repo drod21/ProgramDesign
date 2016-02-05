4b555e27f3f5850857ed46ed30f9356665f47b75
Derek Rodriguez <drod2169@gmail.com>
2016-02-04 20:15:29 -0500

    FINALLY WORKING

--- Projects/Project4/sets.c
+++ Projects/Project4/sets.c
@@ -13,7 +13,6 @@ void selection_sort(int a[], int n);
 int find_largest(int a[], int n);
 
 int main(void) {
-    
     int set_A[SIZE], set_B[SIZE], union_array[SIZE], intersection_array[SIZE];
     int i = 0, f = 0, k = 0, j = 0, elem_A, elem_B;
     
@@ -23,7 +22,6 @@ int main(void) {
     for (i = 0; i < elem_A; i++) {
         scanf("%d", &set_A[i]);
     }
-    
     printf("Please enter the number of elements in the second set:\n");
     scanf("%d", &elem_B);
     
@@ -32,58 +30,61 @@ int main(void) {
         scanf("%d", &set_B[i]);
     }
     
-   //union
-    
-    for (i = 0; i < elem_B; i++) {
-        union_array[k] = set_A[i];
-        k++;
+    //union
+    for (i = 0; i < elem_A; i++) {
+        if (i < elem_A) {
+            union_array[k] = set_A[i];
+            k++;
+        }
     }
     
-    for (i = 0; i < elem_A; i++) {
+    for (i = 0; i < elem_B; i++) {
         f = 0;
+        
         for (j = 0; j < elem_B; j++) {
             if (set_B[i] == union_array[j]) {
                 f = 1;
                 break;
             }
         }
+        
         if (f == 0) {
             if (i < elem_B) {
-            union_array[k] = set_B[i];
-            k++;
+                union_array[k] = set_B[i];
+                k++;
             }
         }
     }
-    
-    selection_sort(union_array, k);
+    //selection_sort(union_array, k);
     printf("The union of the two sets is: { ");
     for (i = 0; i < k; i++) {
         printf(" %d", union_array[i]);
     }
+    
     printf(" }\n");
     
+    k = 0;
     // intersection
-    
     for (j = 0; j < elem_A; j++) {
         for (i = 0; i < elem_B; i++) {
-            if (set_A[i] < set_B[j])
-                intersection_array[i] = set_A[i];
+            if (set_B[i] == set_A[j]) {
+                break;
+            }
         }
+        
         if (i != elem_B) {
             intersection_array[k] = set_B[i];
-            printf("%d",intersection_array[i]);
             k++;
         }
     }
-    
-    selection_sort(intersection_array, k);
+    //selection_sort(intersection_array, k);
     printf("The intersection of the two sets is: { ");
     for (i = 0; i < k; i++) {
         printf(" %d", intersection_array[i]);
     }
     printf(" }\n");
     return 0;
-
+    
 }
 
 void selection_sort(int a[], int n)
