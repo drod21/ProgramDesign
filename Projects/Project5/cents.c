85a3e57c1296c7c723512cbf8e1335cf4ca5c2fd
Derek Rodriguez <drod2169@gmail.com>
2016-02-09 05:59:46 -0500

    Change the cents program to use a separate function and pointers

--- Projects/Project5/cents.c
+++ Projects/Project5/cents.c
@@ -13,28 +13,29 @@ int main()
 {
 	//initialize variables and read input
 	int cents, pennies, quarters, dimes, nickels;
-	pennies = quarters = dimes = nickels =0;
-        printf("Enter the number of cents:\n");
-	scanf("%d", &cents);
-
-	//check the range of the input amount
-	if(cents< 0 || cents > 10000)
-	   printf("Invalid amount %d,\nAmount must be between 0 and 10000, inclusive\n", cents);
-        else {
-		quarters = cents/QUARTER;
-		dimes = cents%QUARTER/DIME;
-		nickels = cents%QUARTER%DIME/NICKEL;
-		pennies = cents%QUARTER%DIME%NICKEL;
+	cents = pennies = quarters = dimes = nickels = 0;
+    coins(cents, &quarters, &dimes, &nickels, &pennies);
 
 		printf("Quarters: %d\n", quarters);
 	        printf("Dimes: %d\n", dimes);
         	printf("Nickels: %d\n", nickels);
 	        printf("Pennies: %d\n", pennies);
-	}
+    
 	return 0;
 }
 
 void coins(int cents, int *quarters, int *dimes, int *nickels, int *pennies) {
+    printf("Enter the number of cents:\n");
+    scanf("%d", &cents);
+    if (cents < 0 || cents > 10000) {
+        printf("Invalid amount %d,\nAmount must be between 0 and 10000, inclusive\n", cents);
+    } else {
+    
+    *quarters = cents / QUARTER;
+    *dimes = cents % QUARTER / DIME;
+    *nickels = cents % QUARTER % DIME / NICKEL;
+    *pennies = cents % QUARTER % DIME % NICKEL;
+    }
     
 }
 
