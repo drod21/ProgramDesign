c520f970faf11921254e736d562350024b241965
Derek Rodriguez <drod2169@gmail.com>
2016-02-25 05:51:49 -0500

    Add largest and invalid option

--- Projects/Project7/numbers.c
+++ Projects/Project7/numbers.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
     char *argv1 = argv[1];
     char *SMALL = "-s";
     char *LARGE = "-l";
-    min = max= 0;
+    min = max = 0;
     
     if (0 == strcmp(argv1, SMALL)) {
         for (i = 2; i < argc; i++) {
@@ -29,6 +29,19 @@ int main(int argc, char *argv[])
             }
         }
         printf("The smallest number is %d\n",min);
-    
+    } else if (0 == strcmp(argv1, LARGE)) {
+        for (i = 2; i < argc; i++) {
+            num = atoi(argv[i]);
+            if(i == 2) {
+                max = num;
+            } else if(max < num) {
+                max = num;
+            }
+        }
+        printf("The largest number is %d\n",max);
+    } else {
+        printf("Invalid option");
+    }
+
         return 0;
 }
