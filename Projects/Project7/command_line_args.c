a7484c5e9c0703b44057c8b8f07250d45a81c8ba
Derek Rodriguez <drod2169@gmail.com>
2016-02-25 05:31:06 -0500

    Begin adding variables for smallest -> largest

---
+++ Projects/Project7/command_line_args.c
@@ -0,0 +1,9 @@
+//
+//  command_line_args.c
+//  
+//
+//  Created by Derek Rodriguez on 2/25/16.
+//
+//
+
+#include <stdio.h>
---
+++ Projects/Project7/numbers.c
@@ -0,0 +1,22 @@
+//
+//  command_line_args.c
+//  
+//
+//  Created by Derek Rodriguez on 2/25/16.
+//
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+    int min, max, num;
+    char *argv1 = argv[1];
+    char *SMALL = "-s";
+    char *LARGE = "-l";
+    min = max= 0;
+        return 0;
+}
