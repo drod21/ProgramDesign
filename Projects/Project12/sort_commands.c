b6e1c3cf5da1103f5613334114a11c46a5807851
Derek Rodriguez <drod2169@gmail.com>
2016-04-20 05:43:26 -0400

    Add condition for args <=1

--- Projects/Project12/sort_commands.c
+++ Projects/Project12/sort_commands.c
@@ -10,3 +10,15 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    /* Checks for command line args <= 1,
+     *if no args, return error message and exit the program */
+    if (argc <= 1) {
+        printf("Invalid option. -l for largest number or -s for smallest\n");
+        printf("number followed by ten numbers.\n");
+        return 0;
+    }
+
+
+
+    return 0;
+}
