11dce0c5dcd5e92a55f922b9f5930282d2a60391
Derek Rodriguez <drod2169@gmail.com>
2016-04-21 07:15:41 -0400

    Sort command line arguments with qsort

--- Projects/Project12/sort_commands.c
+++ Projects/Project12/sort_commands.c
@@ -1,24 +1,37 @@
-//
-//  sort_commands.c
-//  
-//
-//  Created by Derek Rodriguez on 4/20/16.
-//
-//
+/* Derek Rodriguez
+ * U37516832
+ * 4/21/16
+ * sorts a series of words as command-line arguments
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int str_comp(const void *p, const void *q);
 
 int main(int argc, char *argv[]) {
+    int i = 0;
+    
     /* Checks for command line args <= 1,
      *if no args, return error message and exit the program */
     if (argc <= 1) {
-        printf("Invalid option. -l for largest number or -s for smallest\n");
-        printf("number followed by ten numbers.\n");
+        printf("Invalid option.\n");
         return 0;
     }
-
-
-
+    
+    /* Sort the arguments with qsort */
+    qsort(argv, argc, sizeof(*argv), str_comp);
+    
+    for (i = 1; i < argc; i++) {
+        printf("%s ", argv[i]);
+    }
+    printf("\n");
+    
     return 0;
+}
+
+int str_comp(const void *p, const void *q) {
+    /* Use strcmp to compare the strings */
+    return strcmp(*(char **)p, *(char **)q);
 }
