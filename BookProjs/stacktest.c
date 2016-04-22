9d48fb16da709b0021f85d8aab99ddee72d2ce14
Derek Rodriguez <drod2169@gmail.com>
2016-04-22 06:55:48 -0400

    Progress on rolling. TODO: Fix only storing two integers

---
+++ BookProjs/stacktest.c
@@ -0,0 +1,9 @@
+//
+//  stacktest.c
+//  
+//
+//  Created by Derek Rodriguez on 4/21/16.
+//
+//
+
+#include <stdio.h>
--- ClassExercises/Week15/stack.c
+++ ClassExercises/Week15/stack.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include "stack.h"
 
-struct node* push(struct node* top, int i)
-{
+struct node* push(struct node* top, int i) {
    struct node *new_node;
+    
    new_node = malloc(sizeof(struct node));
-   if(new_node == NULL)
-   {
+    
+   if(new_node == NULL) {
 	printf("malloc failed");
         return top;
    }
@@ -18,25 +18,58 @@ struct node* push(struct node* top, int i)
 
 }
 
-struct node *make_empty(struct node *top)
-{
-    top = 0;
-  return top;
+struct node *make_empty(struct node *top) {
+    struct node *p;
+    while (top != NULL) {
+        p = top;
+        top = top->next;
+        free (p);
+    }
+    return NULL;
 }
 
-struct node *pop(struct node *top, int *i)
-{
+struct node *pop(struct node *top, int *i) {
     struct node *temp;
-    *i = top->value;
-    temp = top;
-    top = top->next;
-    free(temp);
     
+    if (top != NULL) {
+        *i = top->value;
+        temp = top->next;
+        free(top);
+        return temp;
+    } else {
+        printf("Stack is empty, can not pop.\n");
+    }
   return top;
 }
 
-void print_stack(struct node *top)
-{
+struct node *roll(struct node *top) {
+    struct node *temp, *new_node;
+    int i;
+    
+    new_node = malloc(sizeof(struct node));
+    if (new_node == NULL) {
+        printf("malloc failed\n");
+        return top;
+    }
+    
+    if (top != NULL) {
+        new_node = top;
+        i = new_node->value;
+        temp = new_node->next;
+        new_node = temp->next;
+        new_node->next->value = i;
+        return new_node;
+    }
+    
+    
+    /*if (top != NULL) {
+    temp = top->next;
+    top->next = temp->next;
+    }*/
+    
+    return top;
+}
+void print_stack(struct node *top) {
    struct node *p;
    if(top != NULL){
       for(p = top; p !=NULL; p=p->next)
