cf910d98d303e7b53e97880f845d7bd8d21c92be
Derek Rodriguez <drod2169@gmail.com>
2016-08-22 08:42:40 -0400

    Finish Project 1

--- Project1/holy_day.c
+++ Project1/holy_day.c
@@ -12,36 +12,46 @@
 
 int main(void) {
     
-    int day, D;
+    int day, D, dayDue;
     
     printf("Please enter todays day (0 = Sunday, 1 = Monday..) 0-6: \n");
     scanf("%d", &day);
-    if (day > 6) {
-        printf("Please enter a day between 0-6");
+    if (day < 0 || day > 6) {
+        printf("Invalid input. The day should be in the range of 0 to 6.\n");
+        return 0;
     } else {
     printf("Please enter the number of days to do the work: \n");
-    scanf("%d", &D);
+    scanf("%d", &dayDue);
+        
+        D = (day + dayDue) % 7;
+        
     
-    //Calculate number of days based on todays day
-    switch(day) {
-        case 0: printf("Sunday");
+    //Calculate number of days based on today's day
+    switch(D) {
+        case 0: printf("The work is due on Monday. The number of days until its due is %d.\n", dayDue + 1);
             break;
-        case 1: printf("Monday");
+            
+        case 1: printf("The work is due on Monday. The number of days until its due is %d.\n", dayDue);
             break;
-        case 2: printf("Tuesday");
+            
+        case 2: printf("The work is due on Tuesday. The number of days until its due is %d.\n", dayDue);
             break;
-        case 3: printf("Wednesday");
+            
+        case 3: printf("The work is due on Wednesday. The number of days until its due is %d.\n", dayDue);
             break;
-        case 4: printf("Thursday");
+            
+        case 4: printf("The work is due on Thursday. The number of days until its due is %d.\n", dayDue);
             break;
-        case 5: printf("Friday");
+            
+        case 5: printf("The work is due on Monday. The number of days until its due is %d.\n", dayDue + 3);
             break;
-        case 6: printf("Saturday");
+        
+        case 6: printf("The work is due on Monday. The number of days until its due is %d.\n", dayDue + 2);
             break;
+        
     }
-    
+        
     }
-    
     
     
     return 0;
