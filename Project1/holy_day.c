83208926972b7e7ddf585ea1f281e4cc45d4628a
Derek Rodriguez <drod2169@gmail.com>
2016-08-22 05:48:09 -0400

    Begin Project 1 Part 2

---
+++ Project1/holy_day.c
@@ -0,0 +1,21 @@
+//
+//  Derek Rodriguez
+//  U37516832
+//  8/22/2016
+//
+//  Calculates the day of the week on which the work would be due, skips over holy days and goes to the next
+//  Monday.
+//
+
+#include <stdio.h>
+
+
+int main(void) {
+    
+    
+    
+    
+    
+    return 0;
+    
+}
