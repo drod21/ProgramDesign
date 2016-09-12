63117232122134fb27e35c8a26050a5eba419742
Derek Rodriguez <drod2169@gmail.com>
2016-09-12 07:08:19 -0400

    Read a message, then print the reverse

--- StudyForMidterm/reversewords.c
+++ StudyForMidterm/reversewords.c
@@ -7,15 +7,34 @@
 //
 
 #include <stdio.h>
-
+#define SIZE 50
 
 int main() {
     
     char ch;
-    int i;
+    char message[SIZE];
+    int i, count = 0, *p;
+    
+    printf("Please enter a sentence to be reversed: \n");
+    
+    while ((ch = getchar()) != '\n') {
+        
+        message[count++] = ch;
+        
+        
+    }
+    
+    /* With pointers */
     
     
     
+    for (i = count - 1; i >= 0; i--) {
+        
+        printf("%c", message[i]);
+        
+    }
+    
+    printf("\n");
     
     return 0;
 }
