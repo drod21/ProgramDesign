914a927d1b02c3dad102e7afa2fedab24ca55a47
Derek Rodriguez <drod2169@gmail.com>
2016-09-13 06:53:15 -0400

    Use pointers as array names

--- StudyForMidterm/reversewords.c
+++ StudyForMidterm/reversewords.c
@@ -13,10 +13,29 @@ int main() {
     
     char ch;
     char message[SIZE], *p;
-    int count = 0;
+   // int count = 0;
     
     printf("Please enter a sentence to be reversed: \n");
     
+    /* Pointers as Array Names */
+    p = message;
+    
+    while ((ch = getchar()) != '\n') {
+        
+        *(p++) = ch;
+        
+        
+    }
+    
+    
+    for (p--; p >= message; p--) {
+        
+        printf("%c", *p);
+        
+    }
+
+    /* With pointers */
+    /*
     while ((ch = getchar()) != '\n') {
         
         message[count++] = ch;
@@ -24,13 +43,13 @@ int main() {
         
     }
     
-    /* With pointers */
+    
     
     for (p = message + count - 1; p >= message; p--) {
         
         printf("%c", *p);
         
-    }
+    }*/
     /*
     for (i = count - 1; i >= 0; i--) {
         
