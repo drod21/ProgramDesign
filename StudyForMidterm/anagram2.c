26bddc050a31c5851c36e6b09f9c0f2ebf0e6d75
Derek Rodriguez <drod2169@gmail.com>
2016-09-26 10:24:11 -0400

    Add are_anagrams function to check and return 0 (false) or 1 (true) if the words are anagrams

---
+++ StudyForMidterm/anagram2.c
@@ -0,0 +1,105 @@
+//
+//  anagram2.c
+//  
+//  Ch 13 PP 14
+//  Created by Derek Rodriguez on 9/26/16.
+//
+//
+
+#include <stdio.h>
+#include <string.h>
+#define N 26
+
+int are_anagrams(const char *word1, const char *word2);
+
+int main()
+{
+    
+    char word1[N] = {0}, word2[N] = {0};
+    char ch;
+    int i;
+    int result;
+    
+    printf("enter a word: ");
+    
+    while((ch=getchar())!= '\n')
+        
+    {
+        word1[ch - 'a']++;
+        
+    }
+    
+    for(i =0;i<N;i++)
+        printf("%d", word1[i]);
+    
+    printf("\n");
+    
+    
+    printf("enter the second word: ");
+    while((ch=getchar())!= '\n')
+        
+    {
+        word2[ch - 'a']++;
+        
+    }
+    
+    for(i = 0; i < N; i++)
+        printf("%d", word2[i]);
+    
+    
+    printf("\n");
+    result = are_anagrams(word1, word2);
+    printf("Result is: %d\n", result);
+    
+    if(result == 1)
+        printf("The words are anagrams.\n");
+    else
+        
+        printf("The words are NOT anagrams.\n");
+    
+    return 0;
+}
+
+int are_anagrams(const char *word1, const char *word2) {
+    
+    int count = 0;
+    int len1, len2;
+    
+    len1 = strlen(word1);
+    len2 = strlen(word2);
+
+    if (len1 != len2) {
+        
+        return 0;
+        
+    }
+    
+    while (*word1 != '\0' && *word2 != '\0') {
+
+    
+        if (*word1 == *word2) {
+            
+            count++;
+            
+            word1++;
+            word2++;
+            
+        }
+    }
+    
+    
+    if (count == 1)
+        
+        return 1;
+    
+    else
+        
+        return 0;
+    
+    
+}
+
+
+
+
+
